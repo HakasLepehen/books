97870f814fcea127a3683d866a4c4e3d93d20dd2
Pavel Petrov <doctorrosen2016@gmail.com>
2025-06-24 00:08:49 +0600

    added search functional

--- src/app/components/books-list/books-list.component.ts
+++ src/app/components/books-list/books-list.component.ts
@@ -1,26 +1,34 @@
-import {Component, Signal} from '@angular/core';
+import {Component, signal, Signal} from '@angular/core';
 import {BookService} from "../../services/book.service";
 import {IBook} from "../../models/IBook";
 import {MatGridList, MatGridTile} from "@angular/material/grid-list";
 import {BookCardComponent} from "../book-card/book-card.component";
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import {MatFormField, MatInput, MatLabel} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-books-list',
   standalone: true,
   imports: [
+    FormsModule,
     MatGridTile,
     MatGridList,
     BookCardComponent,
     MatIcon,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatFormField,
+    MatLabel,
+    MatInput,
   ],
   templateUrl: './books-list.component.html',
   styleUrl: './books-list.component.css'
 })
 export class BooksListComponent {
+  searchQuery = '';
+  filteredBooks = signal<IBook[]>([]);
   books!: Signal<IBook[]>;
 
   constructor(private bookService: BookService) {
@@ -29,5 +37,23 @@ export class BooksListComponent {
 
   addBook() {
     this.bookService.openAddBookPopup();
+  }
+
+  onSearch() {
+    if (!this.searchQuery) {
+      this.filteredBooks.set(this.books());
+      return;
+    }
+    const query = this.searchQuery.toLowerCase();
+    this.filteredBooks.set(
+      this.books().filter(book =>
+        book.title.toLowerCase().includes(query)
+      )
+    );
+  }
+
+  clearSearch() {
+    this.searchQuery = '';
+    this.filteredBooks.set(this.books());
   }
 }
